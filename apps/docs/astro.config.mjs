03c4f4d90dbe48165288a4b4edcfbe380a5697a5
Lucas Kennedy <lukakfp@gmail.com>
2024-10-04 20:21:19 -0300

    chore: Add tailwind integration

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,28 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "Elementrix Docs",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/lucaskfp/elementrix",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
+			customCss: ["./src/tailwind.css"],
+		}),
+		tailwind({
+			applyBaseStyles: false,
 		}),
 	],
 });
